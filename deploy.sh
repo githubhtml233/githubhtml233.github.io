134bc720bd5752b99c3af80cee05fdee10477094
Levi <657349962@qq.com>
2024-06-03 23:17:01 +0800

    feat: update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@
 set -e
 
 # 生成静态文件
-vuepress build
+export NODE_OPTIONS=--openssl-legacy-provider
+NODE_OPTIONS=--openssl-legacy-provider npx vuepress build
+
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:githubhtml233/githubhtml233.github.io.git main:gh-pages
